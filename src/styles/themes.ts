bdcba4ec204a759885ae8438d7ecfd61a70c71fd
jaredvu <jaredvu@gmail.com>
2022-07-15 14:19:00 -0700

    Migrate to pnpm

---
+++ src/styles/themes.ts
@@ -0,0 +1,34 @@
+// Tentative themes in progress
+
+const ThemeShapes = {
+  borderRadiusCircle: '50%',
+  borderRadiusToast: '0.375rem',
+
+};
+
+export const DefaultTheme = {
+  layer1: '#393953',
+  layer2: '#2f2f45',
+  layer3: '#232334',
+  layer4: '#1a1a27',
+  layer5: '#151522',
+  layer6: '#12121d',
+
+  loadingbarshine1: '#464662',
+  loadingbarshine2: '#30304c',
+
+  border: '#2d2d3d',
+
+  accent: '#6966ff',
+  positive: '#16b57f',
+  negative: '#e45555',
+  warning: '#ffc846',
+
+  layer3Faded: 'rgba(35, 35, 52, 0.8)',
+  layer1Faded: 'rgba(57, 57, 83, 0.8)',
+  accentFaded: 'rgba(105, 102, 255, 0.8)',
+
+  text1: '#fafafd',
+  text2: '#c8c7d8',
+  text3: '#807e98',
+};
